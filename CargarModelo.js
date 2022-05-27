32d96aa1de10f344118fe8aec0ad62b9f4347745
D7aniel <cohl.daniel@gmail.com>
2022-05-27 02:03:55 -0300

    Modelo

--- CargarModelo.js
+++ CargarModelo.js
@@ -1,9 +1,9 @@
-import { GLTFLoader } from 'https://unpkg.com/three@0.122.0/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from "https://unpkg.com/three@0.122.0/examples/jsm/loaders/GLTFLoader.js";
 
-export function cargarModelo(archivo,objetoVacio){
-    var loader = new GLTFLoader();
-    loader.load(archivo,function(gltf){
-        /*gltf.scene.traverse( function ( child ) {
+export function cargarModelo(archivo, objetoVacio) {
+  var loader = new GLTFLoader();
+  loader.load(archivo, function (gltf) {
+    /*gltf.scene.traverse( function ( child ) {
 			if ( child.isMesh ) {
             //    console.log(child);
                 if(child.name === "Cylinder"){
@@ -12,36 +12,32 @@ export function cargarModelo(archivo,objetoVacio){
                 }
 			}
 		} );*/
-        objetoVacio.add(gltf.scene);
-    });
+    objetoVacio.add(gltf.scene);
+  });
 }
 
-export function cargarModeloConAnimacion(archivo,objetoVacio,mixer){
-    var loader = new GLTFLoader();
-    loader.load(archivo,function(gltf){
-
-        gltf.scene.traverse( function ( child ) {
-			if ( child.isMesh ) {
-
-               child.scale.set(0.001,0.001,0.001);
-               console.log(child);
-            /*    if(child.name === "Cylinder"){
+export function cargarModeloConAnimacion(archivo, objetoVacio, mixer) {
+  var loader = new GLTFLoader();
+  loader.load(archivo, function (gltf) {
+    gltf.scene.traverse(function (child) {
+      if (child.isMesh) {
+        child.scale.set(0.001, 0.001, 0.001);
+        console.log(child);
+        /*    if(child.name === "Cylinder"){
     				//console.log(child.name);
                     child.layers.enable( 1 );
                 }*/
-			}
-		} );
+      }
+    });
 
-
-
-					//const clip = animations[ i ];
-                /*    mixer = new THREE.AnimationMixer( gltf.scene );
+    //const clip = animations[ i ];
+    /*    mixer = new THREE.AnimationMixer( gltf.scene );
 					const action = mixer.clipAction( gltf.animations[0] );
                     action.play();*/
 
-                    //actions[ clip.name ] = action;
+    //actions[ clip.name ] = action;
 
-                    //console.log(gltf.animations[0]);
-                objetoVacio.add(gltf.scene);
-            });
-        }
+    //console.log(gltf.animations[0]);
+    objetoVacio.add(gltf.scene);
+  });
+}
