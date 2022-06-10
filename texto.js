294752c45db42bcb057bb90455b45ed7c519551e
D7aniel <cohl.daniel@gmail.com>
2022-06-10 13:51:17 -0300

    cmabio version

--- texto.js
+++ texto.js
@@ -13,7 +13,7 @@ class Texto {
     this.titulo.style.textAlign = "center";
     this.subtitulo.style.textAlign = "center";
 
-    this.titulo.innerText = "Version v0.14";
+    this.titulo.innerText = "Version v0.17";
     this.subtitulo.innerText = "espere";
 
     this.texto.append(this.titulo);
