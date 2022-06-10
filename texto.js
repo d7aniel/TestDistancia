5996dbe75eeca858ea794dccde9d89772e2ac360
D7aniel <cohl.daniel@gmail.com>
2022-06-10 16:08:25 -0300

    Luces 2

--- texto.js
+++ texto.js
@@ -18,7 +18,7 @@ class Texto {
     this.titulo.style.textAlign = "center";
     this.subtitulo.style.textAlign = "center";
 
-    this.titulo.innerText = "Los Pañuelos siguen rondando luces";
+    this.titulo.innerText = "Los Pañuelos siguen rondando luces2";
     this.subtitulo.innerText = "cargando ubicacion";
 
     this.texto.append(this.titulo);
